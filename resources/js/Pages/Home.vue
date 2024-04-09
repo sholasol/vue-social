372d978a29c4cce77e412aba5121d3aebae460e3
Solomon <sholasolomon@yahoo.com>
2024-04-09 01:14:33 +0100

    Added authenticated layout to the home page UI

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,42 +4,30 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
-defineProps({
-    // canLogin: {
-    //     type: Boolean,
-    // },
-    // canRegister: {
-    //     type: Boolean,
-    // },
-    // laravelVersion: {
-    //     type: String,
-    //     required: true,
-    // },
-    // phpVersion: {
-    //     type: String,
-    //     required: true,
-    // },
-});
+//defineProps({});
 </script>
 
 <template>
     <Head title="LaraVue Social App" />
 
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 py-2 lg:order-1 h-full overflow-hidden">
-            <GroupList />
-        </div>
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 py-2 lg:order-1 h-full overflow-hidden">
+                <GroupList />
+            </div>
 
-        <div class="lg:col-span-3 py-2 lg:order-3 h-full overflow-auto">
-            <FollowingList />
-        </div>
+            <div class="lg:col-span-3 py-2 lg:order-3 h-full overflow-auto">
+                <FollowingList />
+            </div>
 
-        <div
-            class="lg:col-span-6 py-2 lg:order-2 h-full overflow-hidden flex flex-col"
-        >
-            <CreatePost />
-            <PostList class="flex-1" />
+            <div
+                class="lg:col-span-6 py-2 lg:order-2 h-full overflow-hidden flex flex-col"
+            >
+                <CreatePost />
+                <PostList class="flex-1" />
+            </div>
         </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
